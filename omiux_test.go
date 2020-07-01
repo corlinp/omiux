f60be0b603668d128543163461b82d8d5b8577de
Corlin <tornato7@gmail.com>
2020-07-01 01:25:34 -0700

    test, readme

--- omiux_test.go
+++ omiux_test.go
@@ -1,9 +1,18 @@
-# omiux
+package omiux
 
-Omiux is a Go API schema system. Define your API and Omiux will turn it into a router, CLI, and docs. Keep everything in one struct!
+import (
+	"fmt"
+	"testing"
+)
 
-```go
-api := &API{
+
+type User struct {
+	Name string
+	Age int
+}
+
+func TestOmiux(t *testing.T) {
+	api := &API{
 		Name:        "Users API",
 		Description: "Store and list your users here",
 		Host:        "https://user-api.com/",
@@ -50,59 +59,5 @@ api := &API{
 			},
 		},
 	}
-```
-
-`api.GetBlueprint()`:
-
-=== RUN   TestOmiux
-FORMAT: 1A
-HOST: https://user-api.com/
-
-# Users API
-Store and list your users here
-
-## Users [/v1/users{?age,name}]
-
-
-This is the endpoint to add or get users
-
-### Get Users [GET]
-Returns a list of users
-
-+ Parameters
-    + age: `56` (number, optional) - age of the user
-    + name: `bob` (string, optional) - name of the user
-
-+ Request
-
-    + Headers
-
-
-+ Response 200  (application/json)
-
-    + Body
-
-            {
-                "Name": "patrick star",
-                "Age": 34
-            }
-
-
-
-### Add User [POST]
-Create a new user
-
-+ Request (application/json)
-
-    + Body
-
-            {
-                "Name": "sandy cheeks",
-                "Age": 33
-            }
-
-+ Response 200  (application/json)
-
-    + Headers
-
-            X-Request-ID: 01D78XYFJ1PRM1WPBCBT3VHMNV
+	fmt.Println(api.GetBlueprint())
+}
